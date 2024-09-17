e9f863ebbbc6d9d7e9ba2fae67dc3146f2265e7f
suin <suinyeze@gmail.com>
2024-09-17 17:18:10 +0900

    feat: ボタンテキストをより親しみやすい表現に更新
    Signed-off-by: suin <suinyeze@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        もっと猫を！
+        ねこにゃーん
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
